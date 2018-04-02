452b7a7fb57b193549770e74886fe2eefa1309f1
DESKTOP-FPID300\Anita <anitasari.as250@gmail.com>
2018-04-02 22:53:52 +0700

    penambahan blog

---
+++ application/views/home.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>HTML Education Template</title>
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:700%7CMontserrat:400,600" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css"/>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    </head>
+  <body>
+
+    <!-- Header -->
+    <header id="header" class="transparent-nav">
+      <div class="container">
+
+        <div class="navbar-header">
+          <!-- Logo -->
+          <div class="navbar-brand">
+            <a class="logo" href="home">
+              <img src="assets/img/sklh.jpeg" alt="logo">
+            </a>
+          </div>
+          <!-- /Logo -->
+
+          <!-- Mobile toggle -->
+          <button class="navbar-toggle">
+            <span></span>
+          </button>
+          <!-- /Mobile toggle -->
+        </div>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+          <div class="container">
+          <ul class="main-menu nav navbar-nav navbar-right">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="blog">Blog</a></li>
+            <!-- <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li> -->
+          </ul>
+        </div>
+        </nav>
+        <!-- /Navigation -->
+
+      </div>
+    </header>
+    <!-- /Header -->
+
+    <!-- Home -->
+    <div id="home" class="hero-area">
+
+      <!-- Backgound Image -->
+      <div class="bg-image bg-parallax overlay" style="background-image:url(assets/img/bg.png)"></div>
+      <!-- /Backgound Image -->
+
+      <div class="home-wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8">
+              <h1 class="white-text">Wellcome To SMAN 1 KUTOREJO</h1>
+              <p class="lead white-text">BERSATU, BERAKSI DAN BERPRESTASI</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- /Home -->
+
+    <!-- About -->
+    <div class="section">
+
+      <!-- container -->
+      <div class="container">
+
+        <!-- row -->
+        <div class="row">
+
+          <div class="col-md-6">
+            <div class="section-header">
+              <h2>VISI DAN MISI</h2>
+              <!-- <p class="lead">Libris vivendo eloquentiam ex ius, nec id splendide abhorreant.</p> -->
+            </div>
+
+            <!-- feature -->
+            <div class="feature">
+              <!-- <i class="feature-icon fa fa-flask"></i> -->
+              <div class="feature-content">
+                <h4>Visi Sekolah </h4>
+                <p> "BERPRESTASI BERLANDASKAN IMTAQ-IPTEK, SENI DAN LINGKUNGAN”</p>
+              </div>
+            </div>
+            <!-- /feature -->
+
+            <!-- feature -->
+            <div class="feature">
+              <!-- <i class="feature-icon fa fa-users"></i> -->
+              <div class="feature-content">
+                <h4>Misi Sekolah</h4>
+                <p>1. Mewujudkan penghayatan dan pengamalan ajaran agama yang dianut warga sekolah.</p>
+                <p>2. Membudayakan Istighosah, 5 S (Salam, Sapa, Senyum, Santun, dan Salaman) serta kedisiplinan dan ketertiban warga sekolah.</p>
+                <p>3. Mewujudkan pendidikan, pembelajaran, dan pembimbingan  secara :
+                  A = Aktif
+                  K = Kreatif
+                  I = Inovatif
+                  K = Komunikatif</p>
+                <p>Meningkatkan jumlah lulusan yang diterima di Perguruan  Tinggi  Negeri</p>
+                <p>Meningkatkan jumlah warga sekolah berprestasi di bidang akademik dan non-akademik</p>
+                <p>Mewujudkan peserta didik yang memiliki apresiasi terhadap seni dan budaya</p>
+                <p>Menjaga dan melestarikan fungsi lingkungan, serta mencegah pencemaran dan kerusakan lingkungan</p>
+                <p>Mewujudkan warga sekolah yang berbudaya tertib dan disiplin</p>
+                <p>Membangun jiwa semangat nasionalisme dan kebangsaan</p>
+                <p>Menciptakan hubungan yang harmonis antar warga sekolah, alumni, m</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-md-6">
+            <div class="about-img">
+              <img src="assets/img/f1.jpg" alt="">
+            </div>
+          </div>
+
+        </div>
+        <!-- row -->
+
+      </div>
+      <!-- container -->
+    </div>
+    <!-- /About -->
+
+    <!-- Courses -->
+    <div id="courses" class="section">
+
+      <!-- container -->
+      <section id="about">
+      <div class="container">
+
+        <!-- row -->
+        <div class="row">
+          <div class="section-header text-center">
+            <h2>About</h2>
+            <p class="lead">Lingkungan Sekolah Keberadaan SMA Negeri 1 Kutorejo berdiri pada 1992 sangat strategis sebagai wadah pendidikan formal bagi lulusan SLTP Negeri/Swasta dengan batas- batas yang meliputi kecamatan bangsal, Gondang, Pacet, Pungging, Dlanggu dan sekitarnya. SMA Negeri 1 Kutorejo berdiri berdasar Surat Keputusan Mendikbud. RI. Nomor: 0216/O/1992 tanggal 5 Mei 1992 menempati tanah seluas 2,15 Ha berada di Desa Kutorejo, Kecamatan Kutorejo , Jl Lapangan No 2 Kutorejo T. (0321)-511232 Kabupaten Mojokerto. SMA Negeri 1 Kutorejo merupakan sekolah Rintisan Sekolah Standar Nasional-Kategori Mandiri binaan Dirjend Pembinaan SMA mulai tahun 2008 dengan louching pada tahun 2011 dan akan mencoba nemulai melaksanakan Kurikulum Sistem Satuan Kredit Semester guna menjawab tantangan dan paradigma baru Pendidikan seiring upaya peningkatan mutu dan pelayanan pendidikan. Keadaan Sekolah Lembaga pendidikan UPTSP SMAN I KUTOREJO yang berdiri mulai tahun 1992 pada awalnya dan sampai sekarang terdapat 25 rombongan belajar. Menyadari keterbatasan sarana dan dana, SMAN 1 Kutorejo terus berbenah dan menjalin kerja sama dengan berbagai unsur mulai Pemerintah, Masyarakat, Dunia Usaha dll, sehingga berkat kerjasama dan partisipasi semuanya di bawah kepemimpinan awalnya (Sutrisno, BA. Drs.H. Mochammad Ali Ismail MPd, Drs.Suratman. Herni Sudar Peristiwanti, S.Pd, M.Pd. Drs Eka Prasetya, M.Mpd dan sekarang Ir. Drs Samsul Mu'arifin, M.MPd telah berhasil berkembang dan menorehkan banyak prestasi dari jenjang tingkat Kecamatan sampai jenjang tingkat Propinsi, melalui lomba bidang akademis dan non akademis.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+       
+    <!-- Footer -->
+    <footer id="footer" class="section">
+
+      <!-- container -->
+      <div class="container">
+
+        <!-- row -->
+        <div class="row">
+
+          <!-- footer logo -->
+          <div class="col-md-6">
+            <div class="footer-logo">
+              <a class="logo" href="#home">
+                <img src="assets/img/sklh.jpeg" alt="logo">
+              </a>
+            </div>
+          </div>
+          <!-- footer logo -->
+
+          <!-- footer nav -->
+          <div class="col-md-6">
+            <ul class="footer-nav">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+            </ul>
+          </div>
+          <!-- /footer nav -->
+
+        </div>
+        <!-- /row -->
+
+        <!-- row -->
+        <div id="bottom-footer" class="row">
+
+          <!-- social -->
+          <div class="col-md-4 col-md-push-8">
+            <ul class="footer-social">
+<!--               <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a></li>
+              <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+              <li><a href="#" class="youtube"><i class="fa fa-youtube"></i></a></li>
+              <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li> -->
+            </ul>
+          </div>
+          <!-- /social -->
+
+          <!-- copyright -->
+          <div class="col-md-8 col-md-pull-4">
+            <div class="footer-copyright">
+              <span>&copy; Copyright 2018. All Rights Reserved. | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com">Colorlib</a></span>
+            </div>
+          </div>
+          <!-- /copyright -->
+
+        </div>
+        <!-- row -->
+
+      </div>
+      <!-- /container -->
+
+    </footer>
+    <!-- /Footer -->
+
+    <!-- preloader -->
+    <div id='preloader'><div class='preloader'></div></div>
+    <!-- /preloader -->
+
+
+    <!-- jQuery Plugins -->
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+
+  </body>
+</html>
