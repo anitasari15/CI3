9b159bf143627a3444e4ecc2c68fe342b3dede90
Anita Sari <anitasari.as250@gmail.com>
2018-04-03 14:38:30 +0700

    tugas ketiga

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,70 +1,200 @@
 <!DOCTYPE html>
-<html lang="">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Title Page</title>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>HTML Education Template</title>
 
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-	</head>
-	<body>
-		<nav class="navbar-inverse navbar-default" role="navigation">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="home"><img class="img-responsive" alt="Coding" src="assets/img/sklh.jpeg" width="40" height="40" style="margin-top: -10px;"></a>
-				</div>
-		
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse navbar-ex1-collapse">
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="home">Home</a></li>
-						<li><a href="about">About</a></li>
-						<li><a href="../blog">Blog</a></li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</div>
-		</nav>
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:700%7CMontserrat:400,600" rel="stylesheet">
 
-		<div class="container">
-    		<div class="jumbotron" style="background-color:#CCF;">
-        		<div class="page-header" style="color:#066;</h3>">
-            		<h1>Blog</h1>
-            	</div>
-            	<p>HUKUM NEWTON...</p>
-         	</div>
-    	</div>
-		<div class="container text-center">
-			<?php foreach ($artikel as $key): ?>
-				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<table style="margin-bottom: 30px;">
-						<tr>
-							<td>
-								<a href="blog/detail/<?php echo $key->id_blog ?>" style="color: black;">
-									<img src="img/<?php echo $key->image;?>" alt="Image" width="500" height="400">
-									<br>
-									<?php echo $key->judul ?>
-								</a>
-							</td>
-						</tr>
-					</table>
-				</div>
-			<?php endforeach ?>
-		</div>
-	</body>
+    <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css"/>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    </head>
+  <body>
+
+    <!-- Header -->
+    <header id="header" class="transparent-nav">
+      <div class="container">
+
+        <div class="navbar-header">
+          <!-- Logo -->
+          <div class="navbar-brand">
+            <a class="logo" href="home">
+              <img src="assets/img/sklh.jpeg" alt="logo">
+            </a>
+          </div>
+          <!-- /Logo -->
+
+          <!-- Mobile toggle -->
+          <button class="navbar-toggle">
+            <span></span>
+          </button>
+          <!-- /Mobile toggle -->
+        </div>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+          <div class="container">
+          <ul class="main-menu nav navbar-nav navbar-right">
+            <li><a href="home">Home</a></li>
+            <li><a href="home">About</a></li>
+            <li><a href="blog">Blog</a></li>
+            <!-- <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li> -->
+          </ul>
+        </div>
+        </nav>
+        <!-- /Navigation -->
+
+      </div>
+    </header>
+    <!-- /Header -->
+
+    <!-- Home -->
+    <div id="home" class="hero-area">
+
+      <!-- Backgound Image -->
+      <div class="bg-image bg-parallax overlay" style="background-image:url(assets/img/bg.png)"></div>
+      <!-- /Backgound Image -->
+
+      <div class="home-wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8">
+              <h1 class="white-text">Wellcome To SMAN 1 KUTOREJO</h1>
+              <p class="lead white-text">BERSATU, BERAKSI DAN BERPRESTASI</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- /Home -->
+
+    <!-- About -->
+    <div class="section">
+
+      <!-- container -->
+      <div class="container">
+
+        <!-- row -->
+        <div class="row">
+
+          
+            <!-- /feature -->
+
+            <!-- feature -->
+            <div class="feature">
+              <!-- <i class="feature-icon fa fa-users"></i> -->
+             <div class="container text-center">
+      <?php foreach ($artikel as $key): ?>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <table style="margin-bottom: 30px;">
+            <tr>
+              <td>
+                <a href="blog/detail/<?php echo $key->id_blog ?>" style="color: black;">
+                  <img src="img/<?php echo $key->image;?>" alt="Image" width="500" height="400">
+                  <br>
+                  <?php echo $key->judul ?>
+                </a>
+              </td>
+            </tr>
+          </table>
+        </div>
+      <?php endforeach ?>
+    </div>
+       
+    <!-- Footer -->
+    <footer id="footer" class="section">
+
+      <!-- container -->
+      <div class="container">
+
+        <!-- row -->
+        <div class="row">
+
+          <!-- footer logo -->
+          <div class="col-md-6">
+            <div class="footer-logo">
+              <a class="logo" href="#home">
+                <img src="assets/img/sklh.jpeg" alt="logo">
+              </a>
+            </div>
+          </div>
+          <!-- footer logo -->
+
+          <!-- footer nav -->
+          <div class="col-md-6">
+            <ul class="footer-nav">
+              <li><a href="home">Home</a></li>
+              <li><a href="home">About</a></li>
+            </ul>
+          </div>
+          <!-- /footer nav -->
+
+        </div>
+        <!-- /row -->
+
+        <!-- row -->
+        <div id="bottom-footer" class="row">
+
+          <!-- social -->
+          <div class="col-md-4 col-md-push-8">
+            <ul class="footer-social">
+<!--               <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a></li>
+              <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+              <li><a href="#" class="youtube"><i class="fa fa-youtube"></i></a></li>
+              <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li> -->
+            </ul>
+          </div>
+          <!-- /social -->
+
+          <!-- copyright -->
+          <div class="col-md-8 col-md-pull-4">
+            <div class="footer-copyright">
+              <span>&copy; Copyright 2018. All Rights Reserved. | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com">Colorlib</a></span>
+            </div>
+          </div>
+          <!-- /copyright -->
+
+        </div>
+        <!-- row -->
+
+      </div>
+      <!-- /container -->
+
+    </footer>
+    <!-- /Footer -->
+
+    <!-- preloader -->
+    <div id='preloader'><div class='preloader'></div></div>
+    <!-- /preloader -->
+
+
+    <!-- jQuery Plugins -->
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+
+  </body>
 </html>
